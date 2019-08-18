c2c099d3d4673dd4203c0686092c8cc2bb340042
18798827928 <53905394+18798827928@users.noreply.github.com>
2019-08-18 10:36:18 +0800

    测试类中又添加了一行输出语句

--- src/test/java/com/baizhi/Cmfz1581ApplicationTests.java
+++ src/test/java/com/baizhi/Cmfz1581ApplicationTests.java
@@ -13,6 +13,9 @@ public class Cmfz1581ApplicationTests {
     public void contextLoads() {
 
         System.out.println("===1===");
+        
+        System.out.println("===2===");
+        
     }
 
 }
