f8d540a4d5892b4a5529cec085e85d0d95fabe59
yangb <821518802@qq.com>
2019-08-18 10:15:26 +0800

    第一次创建cmfz_158_1基础的项目环境

---
+++ src/main/java/com/baizhi/Cmfz1581Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmfz1581Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cmfz1581Application.class, args);
+    }
+
+}
